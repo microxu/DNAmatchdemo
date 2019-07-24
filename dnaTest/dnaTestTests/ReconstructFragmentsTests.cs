cbd1a4045452f48ee9664dfcb96a48f8f1373dfe
xu <yongchengxu@gmail.com>
2019-07-25 11:06:51 +1200

    fix test

--- dnaTest/dnaTestTests/ReconstructFragmentsTests.cs
+++ dnaTest/dnaTestTests/ReconstructFragmentsTests.cs
@@ -59,14 +59,28 @@ namespace dnatest.Tests
             string actual = reconstructFragments.ReconstructFragmentSentences(l);
             Assert.AreEqual(expected, actual);
         }
-        [TestMethod()]
+       /*[TestMethod()]
         public void ReconstructFragmentTest4()
         {
             List<string> l = new List<string>();
-            IReconstructFragmentsTools reconstructFragmentsTools = new ReconstructFragmentsTools();
+            IReadFragmentFiles readFragmentFiles = new ReadFragmentFiles();
 
-            l = reconstructFragmentsTools.ReadFragmentsFromFile("D:/AllKindofTest/VSTest/dnaGraph/dnaTest/dnaTest/fragments/fragments.txt");
+            l = readFragmentFiles.ReadFragmentsFromFile("D:/AllKindofTest/VSTest/dnaGraph/dnaTest/dnaTest/fragments/fragments.txt");
             Assert.AreEqual(5, l.Count);
+        }*/
+        [TestMethod()]
+        public void ReconstructFragmentTest4()
+        {
+            var fragments = "m quaerat voluptatem.;pora incidunt ut labore et d;, consectetur, adipisci velit;olore magnam aliqua;idunt ut labore et dolore magn;uptatem.;i dolorem ipsum qu;iquam quaerat vol;psum quia dolor sit amet, consectetur, a;ia dolor sit amet, conse;squam est, qui do;Neque porro quisquam est, qu;aerat voluptatem.;m eius modi tem;Neque porro qui;, sed quia non numquam ei;lorem ipsum quia dolor sit amet;ctetur, adipisci velit, sed quia non numq;unt ut labore et dolore magnam aliquam qu;dipisci velit, sed quia non numqua;us modi tempora incid;Neque porro quisquam est, qui dolorem i;uam eius modi tem;pora inc;am al"
+                .Split(';')
+                .ToList();
+
+            IReconstructFragmentsTools reconstructFragmentsTools = new ReconstructFragmentsTools();
+            IReconstructFragments reconstructFragments = new ReconstructFragments(reconstructFragmentsTools);
+            string actual = reconstructFragments.ReconstructFragmentSentences(fragments);
+
+            Assert.AreEqual("Neque porro quisquam est, qui dolorem i dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.", actual);
+
         }
     }
 }
